ccb76b497122bdc53ec66786e8bd44156ef7ccd8
plalcedo <49098213+plalcedo@users.noreply.github.com>
2021-05-03 11:51:01 -0500

    Practicando jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,8 @@
 
 var shown = false;
 
+$(".notificacion").hide();
+
 $(() => {
     console.log("Ready!");
 
@@ -58,4 +60,55 @@ $("#animateElements").click(() => {
         height: '500px',
         borderRadius: '50%'
     }, "fast");
+    $(".cuadro").removeClass("rojo");
+    $(".cuadro").addClass("verde");
 });
+
+$("section").scroll(() => {
+    $("span").css("display", "inline").fadeOut("slow");
+});
+
+$(window).resize(() => {
+    $("#resize").fadeIn("slow");
+    setTimeout(() => {
+        $("#resize").fadeOut("slow");
+    }, 2000);
+});
+
+/*$("#textBox").keydown(() => {
+    $("#keyDown").fadeIn("fast");
+    $("#keyDown").fadeOut("fast");
+});*/
+
+$("#textBox").keypress(() => {
+    $("#keyPressed").fadeIn("fast");
+    $("#keyPressed").fadeOut("fast");
+});
+
+/*$("#textBox").keyup(() => {
+    $("#keyUp").fadeIn("fast");
+    $("#keyUp").fadeOut("fast");
+});*/
+
+$(window).contextmenu(() => {
+    $("#contextMenuNotification").fadeIn("fast");
+    $("#contextMenuNotification").fadeOut("fast");
+});
+
+$(".cuadro").hover(() => {
+    $("#hoverInNotification").fadeIn("fast");
+    $("#hoverInNotification").fadeOut("fast");
+}, () => {
+    $("#hoverOutNotification").fadeIn("fast");
+    $("#hoverOutNotification").fadeOut("fast");
+});
+
+$(".cuadro").dblclick(() => {
+    $("#dblClickNotification").fadeIn("fast");
+    $("#dblClickNotification").fadeOut("fast");
+});
+
+/*$(window).mousemove(() => {
+    $("#mouseMoveNotification").fadeIn("fast");
+    $("#mouseMoveNotification").fadeOut("fast");
+});*/
